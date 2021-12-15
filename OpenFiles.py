75d28dc60c4eb8797f4c7903d22d0e567175c7fc
SergioCarpintero73 <92789674+SergioCarpintero73@users.noreply.github.com>
2021-12-15 15:57:29 +0400

    Update OpenFiles.py

--- OpenFiles.py
+++ OpenFiles.py
@@ -1,51 +1,24 @@
 # Задача №3
-import os
+def sorted_files(list_file):
+    result_task3 = {}
+    for f_name in list_file:
+        path = os.path.join(os.getcwd(), 'sorted', f_name)
+        new_name = f_name
+        with open(path, encoding='utf-8') as f_name:
+            counter = 0
+            for _ in f_name:
+                counter += 1
+            result_task3[new_name] = counter
+        result_task3 =  dict(sorted(result_task3.items(), key=lambda x:x[1]))
+    path1 = os.path.join(os.getcwd(), '', "result.txt")
+    with open(path1, 'w', encoding="utf-8") as new_file:
+        for key, value in result_task3.items():
+            path = os.path.join(os.getcwd(), 'sorted', key)
+            new_file.write(key + "\n")
+            new_file.write(str(value)+"\n")
+            with open(path, encoding="utf-8") as txt:
+                new_file.write(txt.read() + '\n')
 
-
-#На вход принимаем путь к каталогу. Идем в каталог, получаем список файлов с расширением "TXT".
-#Полученные файлы записываем в список
-def get_files():
-    files_list = []
-    for files in os.listdir('.'):
-        if files.endswith('.txt'):
-            files_list.append(files)
-    return files_list
-
-
-def safeguard(file):
-    if 'result.txt' in file:
-        file.remove('result.txt')
-
-#Циклом идем по списку файлов. Читаем их.
-#Создаем словарь ключом является название файла, значение - количество строк
-#Сортируем по значению.
-def get_sorted_dict(files):
-    merged_dict = {}
-    for name in files:
-        with open(name, 'r', encoding='utf-8') as file:
-            section = file.readlines()
-            count = len(section)
-            section.insert(0, f'{count}\n')
-            merged_dict[name] = section
-    merge_sorted = dict(sorted(merged_dict.items(), key=lambda x: x[1]))
-    return merge_sorted
-
-
-
-def write_result(merged_files):
-    with open('result.txt', 'w', encoding='utf-8') as file:
-        for string in merged_files.keys():
-            file.write(f'{string} \n')
-            file.write(' '.join(merged_files.get(string)))
-            file.write('\n')
-
-
-def main():
-    file = get_files()
-    safeguard(file)
-    merge = get_sorted_dict(file)
-    write_result(merge)
-
-
-main()
-
+                
+files_list = ['1.txt', '2.txt', '3.txt']
+sorted_files(files_list)
